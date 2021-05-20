7fad00997b88e05dd44e56b192d0cdf1f0901107
kevin tran <kevin.trancb@gmail.com>
2021-05-20 16:40:27 -0400

    added validations

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,20 +4,35 @@ const asyncHandler = require("express-async-handler");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
 
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+];
+
 router.post(
-  "/",
+  '/',
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
 
     if (!user) {
-      const err = new Error("Login failed");
+      const err = new Error('Login failed');
       err.status = 401;
-      err.title = "Login failed";
-      err.errors = ["The provided credentials were invalid."];
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
       return next(err);
     }
 
@@ -26,7 +41,7 @@ router.post(
     return res.json({
       user,
     });
-  })
+  }),
 );
 
 router.get(
